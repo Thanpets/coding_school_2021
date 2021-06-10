5cca2f7f537e9d1590be48a882023772561e468b
Thanasis Petsinas <petfountas@hotmail.com>
2021-06-10 13:15:47 +0300

    Creation of empty Student,Professor and Course forms.

---
+++ Session 8 Exercise 1/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    public partial class MainForm : Form {
+
+        List<Student> Students = new List<Student>();
+
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+
+            //MessageBox.Show("Hello World!");
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //textBox1.Text = "Hello World!";
+
+            //checkBox1.Checked = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+
+            Student student = new Student();
+            // TODO: 1. ΝΑ ΠΕΡΑΣΩ ΤΟ STUDENT ΟΒJECT ΣΤΗΝ ΦΟΡΜΑ!
+
+            // TODO: OPEN FORM ...
+            StudentForm form = new StudentForm();
+            form.ShowDialog();
+
+
+            //TODO: WHEN FORM THE CLOSES ADD THE STUDENT TO A STUDENT LIST
+            Students.Add(student);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
---
+++ Session 8 Exercise 1/WindowsFormsApp1/StudentForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    public partial class StudentForm : Form {
+
+        // PROPERTIES ????
+        
+
+        // CONSTRUCTOR
+        public StudentForm() {
+            InitializeComponent();
+        }
+
+
+        // METHODS
+        private void StudentForm_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+            // todo: MODIFY THE OBJECT STUDENT -- CLOSE THE FORM!
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            // todo: CANCEL EVERYTHING -- DO NOT MODIFY THE OBJECT!
+        }
+
+    
+    }
+}
